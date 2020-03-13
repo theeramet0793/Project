84eb438f4a76668a9ca2edfb09b81fdae7d4d4ec
theeramet0793 <59278790+theeramet0793@users.noreply.github.com>
2020-03-14 01:31:09 +0700

    Add files via upload

---
+++ example_map.h
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+void map1(int i){
+if(i==0)cout<<"          *         \n";
+if(i==1)cout<<"         ***        \n";
+if(i==2)cout<<"       *******      \n";
+if(i==3)cout<<"          *         \n";
+if(i==4)cout<<"          *         \n";
+if(i==5)cout<<"          *         \n";
+}
