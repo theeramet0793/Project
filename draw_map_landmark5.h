020aa49b5ab136f8aba158e563dd0d64f7820e46
Bsatayu <59277343+Bsatayu@users.noreply.github.com>
2020-03-19 10:47:51 +0700

    project bsty

--- draw_map_landmark5.h
+++ draw_map_landmark5.h
@@ -4,17 +4,17 @@ using namespace std;
 
 void draw_map_5(int i,int &price){
         if(i==0) cout<<"*************************";
-        if(i==1) cout<<"*   ???? = " <<setw(10)<<price<<"   *";
+        if(i==1) cout<<"*Mysterious Wood = " <<setw(5)<<price<<"     *";
          if(i==2)cout<<"*************************";
          if(i==3)cout<<"*                        ";
-         if(i==4)cout<<"*                        ";
-         if(i==5)cout<<"*                        ";
-         if(i==6)cout<<"*                        ";
-         if(i==7)cout<<"*                        ";
-         if(i==8)cout<<"*                        ";
-         if(i==9)cout<<"*                        ";
-         if(i==10)cout<<"*                        ";
-         if(i==11)cout<<"*                        ";
-         if(i==12)cout<<"*                        ";
+         if(i==4)cout<<"*  ___     ___      ___  ";
+         if(i==5)cout<<"* | - |   | - |    | - | ";
+         if(i==6)cout<<"* |_-_|   |_-_|    |_-_| ";
+         if(i==7)cout<<"*   |       |        |   ";
+         if(i==8)cout<<"*   |       |        |   ";
+         if(i==9)cout<<"*   |       |        |   ";
+         if(i==10)cout<<"*  |       |        |   ";
+         if(i==11)cout<<"*  |       |        |   ";
+         if(i==12)cout<<"* _____________________ ";
          if(i==13)cout<<"*                        ";
 }
