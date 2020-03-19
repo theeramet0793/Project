425f94bcf09290927f207b5a9ee7a0f6e8ca5d64
Bsatayu <59277343+Bsatayu@users.noreply.github.com>
2020-03-19 10:49:48 +0700

    project bsty
    ด่านธรรมดา

--- draw_map_landmark7.h
+++ draw_map_landmark7.h
@@ -2,19 +2,19 @@
 #include<iomanip>
 using namespace std;
 
-void draw_map_5(int i,int &price){
+void draw_map_7(int i,int &price){
         if(i==0) cout<<"*************************";
-        if(i==1) cout<<"*   ???? = " <<setw(10)<<price<<"   *";
+        if(i==1) cout<<"*  Volcano = " <<setw(10)<<price<<" *";
          if(i==2)cout<<"*************************";
-         if(i==3)cout<<"*                        ";
-         if(i==4)cout<<"*                        ";
-         if(i==5)cout<<"*                        ";
-         if(i==6)cout<<"*                        ";
-         if(i==7)cout<<"*                        ";
-         if(i==8)cout<<"*                        ";
-         if(i==9)cout<<"*                        ";
-         if(i==10)cout<<"*                        ";
-         if(i==11)cout<<"*                        ";
+         if(i==3)cout<<"*          *****              ";
+         if(i==4)cout<<"*          *vvv*         ";
+         if(i==5)cout<<"*         *V V V*        ";
+         if(i==6)cout<<"*        *V  V  V*       ";
+         if(i==7)cout<<"*       *         *      ";
+         if(i==8)cout<<"*      *           *     ";
+         if(i==9)cout<<"*     *             *    ";
+         if(i==10)cout<<"*   *               *    ";
+         if(i==11)cout<<"*  *                 *   ";
          if(i==12)cout<<"*                        ";
          if(i==13)cout<<"*                        ";
 }
