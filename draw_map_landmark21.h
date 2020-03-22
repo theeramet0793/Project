b81a65f9374de6c7178c87ca32b23c58ad80c821
konglarock <59278025+konglarock@users.noreply.github.com>
2020-03-22 12:12:55 +0700

    Add files via upload

---
+++ draw_map_landmark21.h
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+void draw_map_21(int i,int &price){
+	 if(i==0)cout<<"***************************";
+	 if(i==1)cout<<"* Tokyo Tower = "<<setw(7)<<price<<"   *";
+	 if(i==2)cout<<"***************************";
+	 if(i==3)cout<<"*             |           *";
+	 if(i==4)cout<<"*             |           *";
+	 if(i==5)cout<<"*             |           *";
+	 if(i==6)cout<<"*            |||          *";
+	 if(i==7)cout<<"*            |||          *";
+	 if(i==8)cout<<"*           [|||]         *";
+	 if(i==9)cout<<"*           [|||]         *";
+	if(i==10)cout<<"*           [|||]         *";
+	if(i==11)cout<<"*         [|||||||]       *";
+	if(i==12)cout<<"*           || ||         *";
+	if(i==13)cout<<"*          ||   ||        *";
+	if(i==14)cout<<"*        [||     ||]      *";
+}
+
