79fbac527813402584c3f1c267da76bfa1c9ae5c
GraPhong <59276835+GraPhong@users.noreply.github.com>
2020-04-03 19:41:44 +0700

    Update draw_map_lndmark1.h
    อัพเดท

--- draw_map_lndmark1.h
+++ draw_map_lndmark1.h
@@ -1,21 +1,24 @@
-#include<iostream>
+   #include<iostream>
 #include<iomanip>
 using namespace std;
 
-void draw_map1(int i,int &price){
-	 if(i==0)cout<<"***************************";
-	 if(i==1)cout<<"*  KAME HOUSE = "<<setw(10)<<price<<"  *";
-	 if(i==2)cout<<"***************************";
-	 if(i==3)cout<<"*       //////\\\\\       *";
-	 if(i==4)cout<<"*     ////////\\\\\\\     *";
-	 if(i==5)cout<<"*   //////////\\\\\\\\\   *";
-	 if(i==6)cout<<"*     |     ____     |    *";
-	 if(i==7)cout<<"*     |    |    |    |    *";
-	 if(i==8)cout<<"*     |    |    |    |    *";
-	 if(i==9)cout<<"* ~~~:|____|____|____|:~~ *";
-	if(i==10)cout<<"*   /::::::::::::::: :\   *";
-	if(i==11)cout<<"*  /~~~~~~~~~~~~~~~~~~ \  *";
-	if(i==12)cout<<"*   ~~~         ~~        *";
-	if(i==13)cout<<"*       ~~~         ~~~   *";
-	if(i==14)cout<<"***************************";
-}
+void draw_map_1_1(int i){
+         if(i==0) cout<<"**************************";
+         if(i==1) cout<<"*          START          ";
+          if(i==2)cout<<"**************************";
+          if(i==3)cout<<"*                         ";
+          if(i==4)cout<<"*                         ";
+          if(i==5)cout<<"*                         ";
+          if(i==6)cout<<"*                         ";
+          if(i==7)cout<<"*                         ";
+          if(i==8)cout<<"*                         ";
+          if(i==9)cout<<"*                         ";
+         if(i==10)cout<<"*                         ";
+         if(i==11)cout<<"*                         ";
+         if(i==12)cout<<"*                         ";
+         if(i==13)cout<<"*                         ";
+         if(i==14)cout<<"*                         ";
+     }
+   
+     
+   
