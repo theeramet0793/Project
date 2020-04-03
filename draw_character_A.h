f2e3078732ad191744fcebe5c5ee5a4cecb8c7ff
Bsatayu <59277343+Bsatayu@users.noreply.github.com>
2020-04-03 13:52:06 +0700

    bsty

---
+++ draw_character_A.h
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+void draw_character_A(int i,int &price){
+        if(i==0) cout<<"*************************";
+        if(i==1) cout<<"*      _______          *";
+         if(i==2)cout<<"*     (      |___       *";
+         if(i==3)cout<<"*     (______|___)      *";
+         if(i==4)cout<<"*    |  ^    ^  |       *";
+         if(i==5)cout<<"*   (|    L     |)      *";
+         if(i==6)cout<<"*    |____O_____|    V  *";
+         if(i==7)cout<<"*     ____| |____   /   *";
+         if(i==8)cout<<"*   __|    o     | /    *";
+         if(i==9)cout<<"*   | |    o     |/     *";
+         if(i==10)cout<<"*   | |    o     |      *";
+         if(i==11)cout<<"*   | |____o_____|      *";
+         if(i==12)cout<<"*       ||   ||         *";
+         if(i==13)cout<<"*       ||   ||         *";
+         if(i==14)cout<<"*************************";
+}
