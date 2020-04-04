34414e038888a49f6707524ea766c2596f7925c9
Erzengel792 <tanadol_boat@hotmail.com>
2020-04-04 13:16:00 +0700

    Edited V.2 Erz

---
+++ draw_character_B.h
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+void draw_character_B(int i,int &price){
+        if(i==0) cout<<"*************************";
+        if(i==1) cout<<"*      _________        *";
+         if(i==2)cout<<"*   (            )      *";
+         if(i==3)cout<<"*    |__________|       *";
+         if(i==4)cout<<"*    | .      . |       *";
+         if(i==5)cout<<"*   (|    ___   |)      *";
+         if(i==6)cout<<"*    |__________|       *";
+         if(i==7)cout<<"*   _ ____| |____ _     *";
+         if(i==8)cout<<"*   | |    V     | |    *";
+         if(i==9)cout<<"*   | |          | |    *";
+         if(i==10)cout<<"*  | |__________| |    *";
+         if(i==11)cout<<"*    |_____|____|      *";
+         if(i==12)cout<<"*       ||   ||        *";
+         if(i==13)cout<<"*       ||   ||        *";
+         if(i==14)cout<<"*************************";
+}
