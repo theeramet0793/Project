0e8581c8564ae288c0cbcdc4f1464cb9100df2d4
Bsatayu <59277343+Bsatayu@users.noreply.github.com>
2020-04-04 20:22:22 +0700

    Update draw_map_landmark5.h

--- draw_map_landmark5.h
+++ draw_map_landmark5.h
@@ -2,19 +2,77 @@
 #include<iomanip>
 using namespace std;
 
-void draw_map_5(int i,int &price){
-        if(i==0) cout<<"*************************";
-        if(i==1) cout<<"*Mysterious Wood = " <<setw(5)<<price<<"     *";
-         if(i==2)cout<<"*************************";
-         if(i==3)cout<<"*                        ";
-         if(i==4)cout<<"*  ___     ___      ___  ";
-         if(i==5)cout<<"* | - |   | - |    | - | ";
-         if(i==6)cout<<"* |_-_|   |_-_|    |_-_| ";
-         if(i==7)cout<<"*   |       |        |   ";
-         if(i==8)cout<<"*   |       |        |   ";
-         if(i==9)cout<<"*   |       |        |   ";
-         if(i==10)cout<<"*  |       |        |   ";
-         if(i==11)cout<<"*  |       |        |   ";
-         if(i==12)cout<<"* _____________________ ";
-         if(i==13)cout<<"*                        ";
+void draw_map_5_1(int i,int &price){
+	 if(i==0)cout<<"**************************";
+	 if(i==1)cout<<"* Mysterious Wood = " <<setw(5)<<price<<" ";
+	 if(i==2)cout<<"**************************";
+	 if(i==3)cout<<"*                         ";
+	 if(i==4)cout<<"*                         ";
+	 if(i==5)cout<<"*                         ";
+	 if(i==6)cout<<"*                         ";
+	 if(i==7)cout<<"*                         ";
+	 if(i==8)cout<<"*                         ";
+	 if(i==9)cout<<"*                         ";
+	if(i==10)cout<<"*                         ";
+	if(i==11)cout<<"*                         ";
+	if(i==12)cout<<"*                         ";
+	if(i==13)cout<<"*                         ";
+	if(i==14)cout<<"*                         ";
+}
+
+
+
+void draw_map_5_2(int i,int &price){
+         if(i==0) cout<<"**************************";
+         if(i==1) cout<<"* Mysterious Wood = " <<setw(5)<<price<<" ";
+          if(i==2)cout<<"**************************";
+          if(i==3)cout<<"*                         ";
+          if(i==4)cout<<"*                         ";
+          if(i==5)cout<<"*                         ";
+          if(i==6)cout<<"*          ///\\\\\\         ";
+          if(i==7)cout<<"*         ////\\\\\\\\        ";
+          if(i==8)cout<<"*        /////\\\\\\\\\\       ";
+          if(i==9)cout<<"*        |        |       ";
+         if(i==10)cout<<"*        |  ____  |       ";
+         if(i==11)cout<<"*        |  |  |  |       ";
+         if(i==12)cout<<"*        |  |  |  |       ";
+         if(i==13)cout<<"*        |__|__|__|       ";
+         if(i==14)cout<<"*                         ";
+     } 
+
+
+void draw_map_5_3(int i,int &price){
+         if(i==0) cout<<"**************************";
+         if(i==1) cout<<"* Mysterious Wood = " <<setw(5)<<price<<" ";
+          if(i==2)cout<<"**************************";
+          if(i==3)cout<<"*                         ";
+          if(i==4)cout<<"*                         ";
+          if(i==5)cout<<"*                         ";
+          if(i==6)cout<<"*    ///\\\\\\      ///\\\\\\   ";
+          if(i==7)cout<<"*   ////\\\\\\\\    ////\\\\\\\\  ";
+          if(i==8)cout<<"*  /////\\\\\\\\\\  /////\\\\\\\\\\ ";
+          if(i==9)cout<<"*  |        |  |        | ";
+         if(i==10)cout<<"*  |  ____  |  |  ____  | ";
+         if(i==11)cout<<"*  |  |  |  |  |  |  |  | ";
+         if(i==12)cout<<"*  |  |  |  |  |  |  |  | ";
+         if(i==13)cout<<"*  |__|__|__|  |__|__|__| ";
+         if(i==14)cout<<"*                         ";
+     }
+
+void draw_map_5_4(int i,int &price){
+         if(i==0) cout<<"**************************";
+         if(i==1) cout<<"* Mysterious Wood = " <<setw(5)<<price<<" ";
+          if(i==2)cout<<"**************************";
+          if(i==3)cout<<"*                         ";
+          if(i==4)cout<<"*  ___     ___      ___   ";
+          if(i==5)cout<<"* | - |   | - |    | - |  ";
+          if(i==6)cout<<"* |_-_|   |_-_|    |_-_|  ";
+          if(i==7)cout<<"*   |       |        |    ";
+          if(i==8)cout<<"*   |       |        |    ";
+          if(i==9)cout<<"*   |       |        |    ";
+         if(i==10)cout<<"*   |       |        |    ";
+         if(i==11)cout<<"*   |       |        |    ";
+         if(i==12)cout<<"* _____________________   ";
+         if(i==13)cout<<"*                         ";
+         if(i==14)cout<<"*                         ";
 }
