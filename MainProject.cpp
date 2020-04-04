998805c9536c5a3f08db4e22e507a45e800fd9ab
theeramet0793 <59278790+theeramet0793@users.noreply.github.com>
2020-04-04 21:24:04 +0700

    Add files via upload

--- MainProject.cpp
+++ MainProject.cpp
@@ -2,8 +2,14 @@
 #include<string>
 #include"scene_start.h"
 #include"scene_input_name.h"
+#include"scene_random_queue.h"
 #include<conio.h>
-#include<dos.h>         
+#include<dos.h>    
+#include<ctime> 
+#include<cstdlib> 
+#include"draw_mapAll.h"  
+#include"where_to_go.h" 
+#include"scene_end.h"
 using namespace std;
 int main(){
 	
@@ -43,14 +49,86 @@ int main(){
         	if(i<NumberPlayers){
         	        	cout<<"\nName player "<<i+1<<" : ";
         	            cin>>name_character[i];
-        	            if(name_character[i].size()>10)name_character[i].erase(10,100);
+        	            if(name_character[i].size()>10)name_character[i].erase(8,100);
 			}else getche();
 			
 			
         	system("CLS");
 		}
-
-     
+   
+   //----------------------------------	   
+   // SCENE 3 RANDOM TURN
+            srand(time(0));
+            
+            for(int i=0;i<=20;i++){
+           	int a=rand()%NumberPlayers;
+           	int b=rand()%NumberPlayers;
+           	string temp=name_character[a];
+           	name_character[a] = name_character[b];
+           	name_character[b] = temp;
+		    }
+		    
+           scene_random_queue(name_character);
+           string continues;
+           continues=getch();
+           system("CLS");
+           
+   //----------------------------------	   
+   // SCENE 4 START GAME
+        int status[25]={1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+        int status_support[25]={1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+        int poc[5]={1,1,1,1,1};                                                           //( pc = position of character)
+        int price[25]={10000,1,1,1,100,1,1,1,1,1,1,1,1,1000,1,1,1,1590,1,1,1,1,1,1,1};
+        int cn=1;                                                                         //(  cn =  character number)
+        int turn_overall=1;
+        int re[4]={0,0,0,0};                                                              //   re = real estate
+        int money[4]={100000,100000,100000,100000};
+        int asset[4];                                                                     //   asset = re + money
+        //status[1]=10;
+		   
+		    do{
+		    	  int cm=where_to_go(poc[cn]);                                           //(  cm = change map 1 square)
+			      status[cm]=cn*10;
+			      
+    	          draw_scene(name_character,status,price,0);
+    	          cout<<"\t\t   press enter to random number : ";
+    	          
+    	              string mystr;
+    	              mystr=getch();
+    	              system("CLS");
+    	                          
+    	              int random_number = rand()%12+1;            
+    	              
+    	              status[ cm ] = status_support[cm];
+    	              
+    	              
+    	              poc[cn] += random_number;
+					  poc[cn] %= 16;
+					  cm=where_to_go(poc[cn]);                                        
+    	              
+    	              status_support[ cm ] = status[ cm ];
+    	              
+    	              status[ cm ]  = cn*10;
+    	              
+    	              draw_scene(name_character,status,price,random_number);
+        	          
+        	          status[ cm ] = status_support[ cm ];
+        	          
+      	          string command1;
+        	      cout<<"\t\t   command : ";
+     	          cin>>command1;
+     	          
+     	          cn+=1;
+     	          if(cn>NumberPlayers)cn=1;
+     	          
+     	          if(command1=="exit")break;
+       	          
+                  system("CLS");
+		    }while(true);
+            
+            
+            system("CLS");
+			scene_end();
      
     return 0;
 }
--- draw_character.h
+++ draw_character.h
@@ -1,21 +1,39 @@
-#include<iostream>
+   #include<iostream>
 #include<iomanip>
 using namespace std;
 
-void draw_character1(int i,int &price){
-	 if(i==0)cout<<"***************************"; 
-	 if(i==1)cout<<"*       @@@@@@@@@@@       *";
-	 if(i==2)cout<<"*      @@@@@@@@@@@@@      *";
-	 if(i==3)cout<<"*     @@/  ~    ~ \\@@     *";
-	 if(i==4)cout<<"*   \\\\ @|  â€¢ |  â€¢ |@      *";
-	 if(i==5)cout<<"*    \\\\ |   O     |       *";
- 	 if(i==6)cout<<"*     \\\\|         |       *";
-	 if(i==7)cout<<"*      \\|=========|\\      *";
-	 if(i==8)cout<<"*       |    |â€¢   |\\\\     *";
-	 if(i==9)cout<<"*       |    |â€¢   | \\\\    *";
-	if(i==10)cout<<"*       |---<>----|  \\\\   *"; 
-	if(i==11)cout<<"*       |....!....|       *";
-	if(i==12)cout<<"*         ||   ||         *";
-	if(i==13)cout<<"*         ||   ||         *";
-	if(i==14)cout<<"***************************";
+
+void draw_character1(int i,string &name){
+     if(i==3)cout<<"*                         ";
+	 if(i==4)cout<<"*   @@@@@@@@@@@@@         ";
+	 if(i==5)cout<<"*  @@/  ~    ~ \\@@        ";
+	 if(i==6)cout<<"*\\\\ @|  • |  • |@         ";
+	 if(i==7)cout<<"* \\\\ |   O     |          ";
+ 	 if(i==8)cout<<"*  \\\\|         | "<<setw(8)<<left<<name<<" ";
+	 if(i==9)cout<<"*   \\|=========|\\         ";
+	if(i==10)cout<<"*    |    |•   |\\\\        ";
+	if(i==11)cout<<"*    |    |•   | \\\\       ";
+	if(i==12)cout<<"*    |---<>----|  \\\\      "; 
+	if(i==13)cout<<"*    |....!....|          ";
+	if(i==14)cout<<"*      ||   ||            ";
+
+}
+
+
+void draw_character2(int i,string &name){
+         
+          if(i==3)cout<<"*   (      |___          *";
+          if(i==4)cout<<"*   (______|___)         *";
+          if(i==5)cout<<"*  |  ^    ^  | "<<setw(8)<<left<<name<<" *";
+          if(i==6)cout<<"* (|    L     |)         *";
+          if(i==7)cout<<"*  |____O_____|    V     *";
+          if(i==8)cout<<"*   ____| |____   /      *";
+          if(i==9)cout<<"* __|    o     | /       *";
+         if(i==10)cout<<"* | |    o     |/        *";
+         if(i==11)cout<<"* | |    o     |         *";
+         if(i==12)cout<<"* | |____o_____|         *";
+         if(i==13)cout<<"*     ||   ||            *";
+         if(i==14)cout<<"*     ||   ||            *";
+         
+
 }
