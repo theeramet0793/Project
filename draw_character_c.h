e464ce76e148ebe27221382f1b8c60f3836c3544
konglarock <59278025+konglarock@users.noreply.github.com>
2020-04-04 22:19:38 +0700

    Update draw_character_c.h

--- draw_character_c.h
+++ draw_character_c.h
@@ -2,7 +2,7 @@
 #include<iomanip>
 using namespace std;
 
-void draw_character_c(int i,int &price){
+void draw_character_C(int i,int &price){
         if(i==0) cout<<"*************************";
         if(i==1) cout<<"*        @@@@@@@@       *";
          if(i==2)cout<<"*     @@@@@@@@@@@@@@    *";
@@ -14,7 +14,7 @@ void draw_character_c(int i,int &price){
          if(i==8)cout<<"*   |  |   *****   |  | *";
          if(i==9)cout<<"*   |  | *  ***  * |  | *";
          if(i==10)cout<<"*  |  |____ *_____|  | *";
-         if(i==11)cout<<"* punch_____|____|punch*";
+         if(i==11)cout<<"* punch_____* ____|punch*";
          if(i==12)cout<<"*        ||   ||       *";
          if(i==13)cout<<"*       foot  foot     *";
          if(i==14)cout<<"*************************";
